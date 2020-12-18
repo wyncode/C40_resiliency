7fea383269ae142c7d056b700e793732e5adb675
Jessica Palomo <jesicalynpalomo@gmail.com>
2020-12-17 21:45:31 -0500

    updated await Request.deleteMany

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -164,7 +164,7 @@ userSchema.pre('save', async function (next) {
  */
 userSchema.pre('remove', async function (next) {
   const user = this;
-  await Task.deleteMany({
+  await Request.deleteMany({
     owner: user._id
   });
   next();
