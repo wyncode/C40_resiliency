4ba43a1031ac6ae6020e61aab0a19dc0c0e9e23c
juanmartinez238 <juan.martinez238@yahoo.com>
2021-01-12 03:35:41 -0500

    updated home page and css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,28 +1,64 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import logo from '../assets/img/logo.svg';
+import { Carousel } from 'react-bootstrap';
+import config from '../assets/img/config.png';
 
 function Home() {
   return (
-    <div>
-      <h2>Resiliency Connection</h2>
-
-      <p>
-        Welcome to Resiliency Connection. We are glad you are here. Please let
-        us know if you are here because you are in need of assistance, or if you
-        are here to offer assistance to those in need.
-      </p>
-
-      <h4>
-        I need assistance in the form of Food, Water, Education and/or Health
-      </h4>
-      <Link className="btn btn-primary" to="/usersignup">
-        Click Me
-      </Link>
-      <h4>I am an NGO and can offer assistance</h4>
-      <Link className="btn btn-primary" to="/ngosignup">
-        Click Me
-      </Link>
+    <div className="homeContainer">
+      <div className="homeTop">
+        <div className="banner">
+          <h3>The Resiliency Connection</h3>
+          <p>
+            Welcome to The Resiliency Connection. We want to help you feel
+            supported in the midst of difficulty. If you are in need of
+            assistance, or can provide humanitarian services for people in need,
+            please select your button choice below.
+          </p>
+          <div className="homeButton">
+            <div className="q1">
+              <Link className="btn btn-primary" to="/usersignup">
+                Receive Help
+              </Link>
+            </div>
+            <div className="q2">
+              <Link className="btn btn-primary" to="/ngosignup">
+                Share Help
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="logo-img">
+          <img src={logo} alt="crlogo" className="logo" />
+        </div>
+      </div>
+      <div className="carouselDiv">
+        {/* <Carousel className="carousel">
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src={config} />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src="/carousel.png" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src="/carousel.png" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>; */}
+      </div>
     </div>
   );
 }
